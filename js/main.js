b93d1de6bc28f1e12426adb4cfe2148bc9fc086d
nermeenelmasry286 <112644208+nermeenelmasry286@users.noreply.github.com>
2024-12-23 14:56:14 +0200

    movies websiTe

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+
+var swiper = new Swiper(".popular-content", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  
+  autoplay: {
+      delay: 755500,
+      disableOnInteraction: false,
+  },
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+      280: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+      },
+      320: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+      },
+      758: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+      },
+      900: {
+          slidesPerView: 4,
+          spaceBetween: 20,
+      },
+  }
+});
+
+let playButton = document.querySelector(".play-movie");
+let video = document.querySelector(".video-container");
+let myvideo = document.querySelector("#myvideo");
+let closeBtn = document.querySelector(".close-movie");
+
+playButton.onclick = () => {
+  video.classList.add("show-video");
+  //auto play
+  myvideo.play();
+};
+
+closeBtn.onclick = () => {
+  video.classList.remove("show-video");
+  //pause
+  myvideo.pause();
+};
